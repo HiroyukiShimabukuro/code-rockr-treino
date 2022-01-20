40c9a76d770345cabd19de94adb6c04c6d1b246f
Carlos Hiroyuki Shimabukuro <carlos@digitalone.com.br>
2022-01-20 02:58:56 -0300

    preparando para receber dados dinamicos

--- src/index.js
+++ src/index.js
@@ -1,29 +1,56 @@
-// Dado dois arrays de inteiros não vazios, determine se os valores do segundo array estão na mesma ordem no primeiro array.
+console.log(window.innerHeight);
 
-// public boolean isSubsequent(int[] values, int[] sequence)
+const lista_posts = document.querySelector('main');
 
-// Exemplo entrada
-// isSubsequent(new int[]{5, 1, 22, 25, 6, -1, 8, 10}, new int[]{1, 6, -1, 10})
-// Exemplo saída
-// true
-  const one = [5, 1, 22, 25, 6, -1, 8, 10]
-  const two = [1, 6, -1, 10]
-  
-  let exist = two.filter((el, index)=>{
-    if(one.includes(el))
-    return el;
-  })
-  console.log(exist);
-  console.log(two);
+const posts = `
+  <article class="row">
+    <article class="d-flex pe-0 col-md-6 col-12">
+      <img class="img-peq" src="./img/stage-wide-frontal.png" alt="">
+      <article class="text-center w-100 bg-white">
+        <h3>Kelsi Monahan</h3>
+        <h2>Qui occaecati vero et quibusdam non</h2>
+        <p class="text">Saepe quia culpa vero. Velit numquam corporis nihil sint enim exercitationem. Rem nulla illum sint et id dolore voluptas </p>
+      </article>
+    </article>
+    <article class="col-md-6 col-12 ps-0 d-flex">
+      <img class="img-peq" src="./img/vocal-mic-zoom.png" alt="">
+      <article class="text-center w-100 bg-white">
+        <h3>Kelsi Monahan</h3>
+        <h2>Qui occaecati vero et quibusdam non</h2>
+        <p class="text">Saepe quia culpa vero. Velit numquam corporis nihil sint enim exercitationem. Rem nulla illum sint et id dolore voluptas </p>
+      </article>      
+    </article>
+    </article>
+    <article class="row">
+    <article class="col-offset-4 col-8 d-flex">
+      <img class="img-med" src="./img/guitar-focus.png" alt="">
+      <article class="text-center w-100 bg-white">
+        <h3>Kelsi Monahan</h3>
+        <h2>Qui occaecati vero et quibusdam non</h2>
+        <p class="text">Saepe quia culpa vero. Velit numquam corporis nihil sint enim exercitationem. Rem nulla illum sint et id dolore voluptas </p>
+      </article>
+    </article>
+  </article>
+  `;
 
-  let sequence = exist.some((el,index) =>{
-    
-    if(two[index] == el) el;
-    else 
-    return false;
-  })
-  
-  console.log(sequence);
-  
-  const letras = ['g','g','y','c','c','c','q','c','y','y']
-  console.log(letras);
+  function makeRequest() {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    httpRequest.open('GET', 'https://stormy-shelf-93141.herokuapp.com/');
+    httpRequest.send();
+  }
+
+  function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        alert(httpRequest.responseText);
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
