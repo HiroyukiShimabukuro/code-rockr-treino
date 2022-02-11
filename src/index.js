d5330e374d8962300b7a94162cd3730f7e69f3d4
Carlos Hiroyuki Shimabukuro <carlos@digitalone.com.br>
2022-02-11 01:33:32 -0300

    tentando mostrar os posts corretamente

--- src/index.js
+++ src/index.js
@@ -1,23 +1,24 @@
 var posts_data = [];
 document.onreadystatechange = () => {
-  let page;
-  
+  var page;
+  let multiplePage;
+
   if (document.readyState === 'complete') {
-    window.addEventListener('scroll', function(event){
+    window.addEventListener('scroll', function (event) {
       let element = document.body;
-      if(element.clientHeight - window.scrollY === document.documentElement.clientHeight){
+      if (element.clientHeight - window.scrollY === document.documentElement.clientHeight) {
         console.log('final');
-        page = page+1;
-        queryPage = '&_page='+ page;
+        page = page + 1;
+        queryPage = '&_page=' + page;
         console.log(queryPage);
-        fetch('https://stormy-shelf-93141.herokuapp.com/articles' + '?_limit=3'+queryPage,
+        fetch('https://stormy-shelf-93141.herokuapp.com/articles' + '?_limit=3' + queryPage,
           {
             method: 'GET'
           })
           .then((response) => response.json())
-          .then((responseJson) =>{
+          .then((responseJson) => {
             posts_data.push(responseJson);
-        });
+          });
         mountPost();
       }
     })
@@ -26,49 +27,52 @@ document.onreadystatechange = () => {
 
     function mountPost() {
       const intevalo = setInterval(() => {
-        if(Object.values(posts_data).length != 0)
-        posts_data[0].forEach(element => {
-          posts = `
+        multiplePage = parseInt(page) * 3;
+
+        if (Object.values(posts_data).length != 0) {
+          posts_data[0].forEach(element => {
+            console.log(posts_data[0][0 + multiplePage]);
+            posts += `
           <article class="row">
             <article class="d-flex pe-0 col-md-6 col-12">
-              <img class="img-peq" src="${posts_data[0][0].imageUrl}" alt="">
+              <img class="img-peq" src="${posts_data[0][parseInt(0+multiplePage)].imageUrl}" alt="">
               <article class="text-center w-100 bg-white">
-                <h3>${posts_data[0][0].author}</h3>
-                <h2>${posts_data[0][0].title}</h2>
-                ${posts_data[0][0].article}
+                <h3>${posts_data[0][0 + multiplePage].author}</h3>
+                <h2>${posts_data[0][0 + multiplePage].title}</h2>
+                ${posts_data[0][0 + multiplePage].article}
               </article>
             </article>
           `;
-        });
-        posts += `
+          });
+          posts += `
             <article class="col-md-6 col-12 ps-0 d-flex">
-            <img class="img-peq" src="${posts_data[0][1].imageUrl}" alt="">
+            <img class="img-peq" src="${posts_data[0][1 + multiplePage].imageUrl}" alt="">
             <article class="text-center w-100 bg-white">
-              <h3>${posts_data[0][1].author}</h3>
-              <h2>${posts_data[0][1].title}</h2>
-              ${posts_data[0][1].article}
+              <h3>${posts_data[0][1 + multiplePage].author}</h3>
+              <h2>${posts_data[0][1 + multiplePage].title}</h2>
+              ${posts_data[0][1 + multiplePage].article}
             </article>      
             </article>
           </article>
         `;
-        posts += `
+          posts += `
         <article class="row">
           <article class="col-offset-4 col-8 d-flex">
-            <img class="img-med" src="${posts_data[0][2].imageUrl}" alt="">
+            <img class="img-med" src="${posts_data[0][2 + multiplePage].imageUrl}" alt="">
             <article class="text-center w-100 bg-white">
-              <h3>${posts_data[0][2].author}</h3>
-              <h2>${posts_data[0][2].title}</h2>
-              ${posts_data[0][2].article}
+              <h3>${posts_data[0][2 + multiplePage].author}</h3>
+              <h2>${posts_data[0][2 + multiplePage].title}</h2>
+              ${posts_data[0][2 + multiplePage].article}
             </article>
           </article>
         </article>
         `
-        lista_posts.insertAdjacentHTML('beforeend', posts);
-
+          lista_posts.insertAdjacentHTML('beforeend', posts);
+        }
         clearInterval(intevalo);
 
       }, 750);
-      
+
     }
 
     fetch('https://stormy-shelf-93141.herokuapp.com/articles' + '?_limit=3',
@@ -76,10 +80,10 @@ document.onreadystatechange = () => {
         method: 'GET',
       })
       .then((response) => response.json())
-      .then((responseJson) =>{
+      .then((responseJson) => {
         page = 1;
         posts_data.push(responseJson);
-    });
+      });
     mountPost();
   }
 }
